99cde6dd037b465c63212631b13db98cf89886f9
Rustam Semenov <88612028+SemenovRustam@users.noreply.github.com>
2021-10-17 21:46:53 +0500

    Add files via upload

--- com.RustamSemenov.spring.mvc/src/main/java/com/RustamSemenov/spring/mvc/Employee.java
+++ com.RustamSemenov.spring.mvc/src/main/java/com/RustamSemenov/spring/mvc/Employee.java
@@ -1,12 +1,35 @@
 package com.RustamSemenov.spring.mvc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Employee {
     private  String name;
     private  String surname;
     private  int salary;
     private String department;
+    private  String carBrand;
+
+    private Map<String,String> departmens;
+    private Map<String,String> carBrands;
+    private String [] leanguages;
+    private Map <String, String > list;
 
     public Employee() {
+        departmens = new HashMap<>();
+        departmens.put("IT", "Information technology");
+        departmens.put("Sales", "Sales");
+        departmens.put("HR", "Human resources");
+
+        carBrands = new HashMap<>();
+        carBrands.put("BMW", "BMW");
+        carBrands.put("Audi", "Audi");
+        carBrands.put("Mercedes-Benz", "Mercedes-Benz");
+
+        list = new HashMap<>();
+        list.put("English", "EN");
+        list.put("Deutch", "DE");
+        list.put("French", "FR");
     }
 
     public String getName() {
@@ -39,6 +62,48 @@ public class Employee {
 
     public void setDepartment(String department) {
         this.department = department;
+
+
+    }
+
+    public Map<String, String> getDepartmens() {
+        return departmens;
+    }
+
+    public void setDepartmens(Map<String, String> departmens) {
+        this.departmens = departmens;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public Map<String, String> getCarBrands() {
+        return carBrands;
+    }
+
+    public void setCarBrands(Map<String, String> carBrands) {
+        this.carBrands = carBrands;
+    }
+
+    public String[] getLeanguages() {
+        return leanguages;
+    }
+
+    public void setLeanguages(String[] leanguages) {
+        this.leanguages = leanguages;
+    }
+
+    public Map<String, String> getList() {
+        return list;
+    }
+
+    public void setList(Map<String, String> list) {
+        this.list = list;
     }
 
     @Override
--- com.RustamSemenov.spring.mvc/src/main/webapp/WEB-INF/view/ask-emp-details-view.jsp
+++ com.RustamSemenov.spring.mvc/src/main/webapp/WEB-INF/view/ask-emp-details-view.jsp
@@ -26,6 +26,25 @@
     Salary<form:input path="salary"/>
     <br>
     <br>
+
+    Department <form:select path="department">
+   <form:options items="${employee.departmens}"/>
+</form:select>
+    <br>
+    <br>
+
+    Which   car do you want?
+
+   <form:radiobuttons path="carBrand" items="${employee.carBrands}"/>
+
+    <br>
+    <br>
+
+    Foreign Laanguage (s)
+
+    <form:checkboxes path="leanguages" items="${employee.list}"/>
+
+
     <input type="submit" value="OK">
 
 
